38464e102eb46e01d78ebc2b3f20976661a94689
Igor de Almeida <igoor1205@gmail.com>
2021-12-23 12:03:38 -0300

    :rotating_light: Add ThemeProvider for tests

---
+++ src/utils/tests.tsx
@@ -0,0 +1,16 @@
+import { NativeBaseProvider } from 'native-base'
+import React from 'react'
+import { nbTheme } from '@/themes'
+
+export const ThemeProvider: React.FC = ({ children }) => {
+  const inset = {
+    frame: { x: 0, y: 0, width: 0, height: 0 },
+    insets: { top: 0, left: 0, right: 0, bottom: 0 },
+  }
+
+  return (
+    <NativeBaseProvider initialWindowMetrics={inset} theme={nbTheme}>
+      {children}
+    </NativeBaseProvider>
+  )
+}
