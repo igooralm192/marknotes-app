663aa0c0f4eec0ea92bb5f72953cfc220a04726d
Igor de Almeida <igoor1205@gmail.com>
2021-12-23 18:19:29 -0300

    :rotating_light: Mock redux-persist

--- jest/setup.ts
+++ jest/setup.ts
@@ -12,4 +12,13 @@ jest.mock('@react-navigation/native', () => {
 
 jest.mock('@react-native-async-storage/async-storage', () => MockAsyncStorage)
 
+jest.mock('redux-persist', () => {
+  const persistActual = jest.requireActual('redux-persist')
+  return {
+    ...persistActual,
+    persistReducer: jest.fn((_, reducers) => reducers),
+    persistStore: jest.fn(),
+  }
+})
+
 export {}
