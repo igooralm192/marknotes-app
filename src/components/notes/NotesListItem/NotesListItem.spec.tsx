5590ecbb4891a9e6920e1a0d246089842f852560
Igor de Almeida <igoor1205@gmail.com>
2021-12-23 12:04:06 -0300

    :rotating_light: Add NotesListItem tests

---
+++ src/components/notes/NotesListItem/NotesListItem.spec.tsx
@@ -0,0 +1,41 @@
+import { fireEvent, render } from '@testing-library/react-native'
+import React from 'react'
+
+import { Note } from '@/types'
+import { ThemeProvider } from '@/utils'
+import NotesListItem from '.'
+
+const note: Note = {
+  id: 'any_id',
+  title: 'any_title',
+  content: 'any_content',
+  date: new Date(),
+}
+
+const onDeleteNoteMock = jest.fn()
+
+describe('HomeScreen', () => {
+  it('should renders correctly', () => {
+    const screen = render(
+      <NotesListItem {...note} onDeleteNote={onDeleteNoteMock} />,
+      {
+        wrapper: ThemeProvider,
+      },
+    )
+
+    expect(screen.getByText(note.title)).toBeDefined()
+    expect(screen.getByText(note.content)).toBeDefined()
+  })
+
+  it('should call method on delete icon press', () => {
+    const screen = render(
+      <NotesListItem {...note} onDeleteNote={onDeleteNoteMock} />,
+      {
+        wrapper: ThemeProvider,
+      },
+    )
+
+    fireEvent.press(screen.getByTestId('delete-note-button'))
+    expect(onDeleteNoteMock).toHaveBeenCalledWith(note.id)
+  })
+})
--- src/components/notes/NotesListItem/index.tsx
+++ src/components/notes/NotesListItem/index.tsx
@@ -4,16 +4,27 @@ import { Note } from '@/types'
 
 import { Container, Title, Content, DeleteButton, DeleteIcon } from './styles'
 
-export interface NotesListItemProps extends Note {}
+export interface NotesListItemProps extends Note {
+  onDeleteNote: (noteId: string) => void
+}
 
-const NotesListItem: React.FC<NotesListItemProps> = ({ title, content }) => {
+const NotesListItem: React.FC<NotesListItemProps> = ({
+  id,
+  title,
+  content,
+  onDeleteNote,
+}) => {
   return (
     <Container>
       <Title>{title}</Title>
 
       <Content>{content}</Content>
 
-      <DeleteButton icon={<DeleteIcon />} />
+      <DeleteButton
+        onPress={() => onDeleteNote(id)}
+        icon={<DeleteIcon />}
+        testID="delete-note-button"
+      />
     </Container>
   )
 }
