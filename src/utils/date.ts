dac3aad1d028eca11b508305fec222dda1d1faa0
Igor de Almeida <igoor1205@gmail.com>
2021-12-24 12:39:48 -0300

    :bug: Adjust formatDate to use locale pt-br

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,8 @@
 import { format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 
 export const formatDate = (date: Date): string => {
-  return format(date, 'dd de LLLL de yyyy')
+  return format(date, "dd 'de' LLLL 'de' yyyy', às 'HH:mm'h'", {
+    locale: ptBR,
+  })
 }
