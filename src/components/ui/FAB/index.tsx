a99fe9031017ed9ec0bee77b53196dd1ae0c6fdd
Igor de Almeida <igoor1205@gmail.com>
2021-12-24 09:25:56 -0300

    :bug: Remove background from FAB component

--- src/components/ui/FAB/index.tsx
+++ src/components/ui/FAB/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { FABProps } from './types'
 
 const FAB: React.FC<FABProps> = props => {
-  return <Fab right={6} bottom={12} bg="primary.500" {...props} />
+  return <Fab right={6} bottom={12} {...props} />
 }
 
 export default FAB
