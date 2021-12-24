e006da416080d578670364d65cd823d7aaa19ce8
Igor de Almeida <igoor1205@gmail.com>
2021-12-24 12:40:17 -0300

    :bug: Adjust font props of Input component

--- src/components/ui/Input/index.tsx
+++ src/components/ui/Input/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { InputProps } from './types'
 
 const Input: React.FC<InputProps> = props => {
-  return <NBInput px={4} fontSize={14} {...props} />
+  return <NBInput px={4} fontSize="md" fontFamily="body" {...props} />
 }
 
 export default Input
