4ba022ae62b1a55a0a9d615195e70e4c36ddb1e5
Igor de Almeida <igoor1205@gmail.com>
2021-12-26 10:44:25 -0300

    :wrench: Configure styled helper to accept components with ref

--- src/components/ui/Input/index.tsx
+++ src/components/ui/Input/index.tsx
@@ -1,10 +1,14 @@
 import { Input as NBInput } from 'native-base'
 import React from 'react'
+import { TextInput } from 'react-native'
 
 import { InputProps } from './types'
 
-const Input: React.FC<InputProps> = props => {
-  return <NBInput px={4} fontSize="md" fontFamily="body" {...props} />
+const Input: React.ForwardRefRenderFunction<TextInput, InputProps> = (
+  props,
+  ref,
+) => {
+  return <NBInput ref={ref} px={4} fontSize="md" fontFamily="body" {...props} />
 }
 
-export default Input
+export default React.forwardRef(Input)
--- src/utils/styled.ts
+++ src/utils/styled.ts
@@ -1,7 +1,59 @@
 import React from 'react'
 
-export function styled<T>(Component: React.FC<T>, props?: Partial<T>) {
-  return function (newProps: T) {
-    return React.createElement<T>(Component, { ...props, ...newProps })
+type FCProps<T> = T
+type RefFCProps<T, P> = T & React.RefAttributes<P> & { isRef?: boolean }
+
+type FC<T> = React.FC<FCProps<T>>
+type RefFC<T, P> = React.ForwardRefExoticComponent<RefFCProps<T, P>>
+
+function haveRef<T, P>(
+  Component: FC<T> | RefFC<T, P>,
+): Component is RefFC<T, P> {
+  return (
+    (Component as RefFC<T, P>)?.$$typeof?.toString() ===
+    'Symbol(react.forward_ref)'
+  )
+}
+
+// Functional Component without 'ref'
+export function styled<T>(
+  Component: FC<T>,
+  props?: Partial<FCProps<T>>,
+): (newProps: FCProps<T>) => React.FunctionComponentElement<T>
+
+// Functional Component with 'ref'
+export function styled<T, P>(
+  Component: RefFC<T, P>,
+  props?: Partial<RefFCProps<T, P>>,
+): (newProps: RefFCProps<T, P>) => React.FunctionComponentElement<T>
+
+export function styled<T, P>(
+  Component: FC<T> | RefFC<T, P>,
+  props?: Partial<FCProps<T>> | Partial<RefFCProps<T, P>>,
+) {
+  if (haveRef(Component)) {
+    return styledRefFC<T, P>(Component as RefFC<T, P>, props)
+  } else {
+    return styledFC<T>(Component as FC<T>, props)
   }
+}
+
+function styledFC<T>(Component: FC<T>, props?: Partial<FCProps<T>>) {
+  return function (newProps: FCProps<T>) {
+    return React.createElement(Component, {
+      ...props,
+      ...newProps,
+    })
+  }
+}
+
+function styledRefFC<T, P>(
+  Component: RefFC<T, P>,
+  props?: Partial<RefFCProps<T, P>>,
+) {
+  function NewComponent(newProps: T, ref: React.ForwardedRef<P>) {
+    return React.createElement<T>(Component, { ...props, ...newProps, ref })
+  }
+
+  return React.forwardRef(NewComponent)
 }
