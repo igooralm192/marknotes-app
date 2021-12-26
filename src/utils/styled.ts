41f63fcb339ca3828b3d2cade0a3904a6a249af6
Igor de Almeida <igoor1205@gmail.com>
2021-12-26 11:47:21 -0300

    :dirty: Improve styled helper with attrs callback

--- src/utils/styled.ts
+++ src/utils/styled.ts
@@ -1,10 +1,15 @@
 import React from 'react'
 
 type FCProps<T> = T
-type RefFCProps<T, P> = T & React.RefAttributes<P> & { isRef?: boolean }
+type RefFCProps<T, P> = React.PropsWithoutRef<T> & React.RefAttributes<P>
 
 type FC<T> = React.FC<FCProps<T>>
 type RefFC<T, P> = React.ForwardRefExoticComponent<RefFCProps<T, P>>
+
+type StyledAttrs<T> = Partial<T> | ((props: T) => Partial<T>)
+
+type StyledFCAttrs<T> = StyledAttrs<FCProps<T>>
+type StyledRefFCAttrs<T, P> = StyledAttrs<RefFCProps<T, P>>
 
 function haveRef<T, P>(
   Component: FC<T> | RefFC<T, P>,
@@ -15,44 +20,67 @@ function haveRef<T, P>(
   )
 }
 
+function computeAttrs<T>(props: T, attrs?: StyledAttrs<T>) {
+  let computedAttrs: Partial<T>
+
+  if (typeof attrs === 'function') {
+    computedAttrs = attrs(props)
+  } else {
+    computedAttrs = attrs ?? {}
+  }
+
+  return computedAttrs
+}
+
 // Functional Component without 'ref'
-export function styled<T>(
-  Component: FC<T>,
-  props?: Partial<FCProps<T>>,
-): (newProps: FCProps<T>) => React.FunctionComponentElement<T>
+export function styled<T>(Component: FC<T>, attrs?: StyledFCAttrs<T>): FC<T>
 
 // Functional Component with 'ref'
 export function styled<T, P>(
   Component: RefFC<T, P>,
-  props?: Partial<RefFCProps<T, P>>,
-): (newProps: RefFCProps<T, P>) => React.FunctionComponentElement<T>
+  attrs?: StyledRefFCAttrs<T, P>,
+): RefFC<T, P>
 
 export function styled<T, P>(
   Component: FC<T> | RefFC<T, P>,
-  props?: Partial<FCProps<T>> | Partial<RefFCProps<T, P>>,
+  attrs?: StyledFCAttrs<T> | StyledRefFCAttrs<T, P>,
 ) {
   if (haveRef(Component)) {
-    return styledRefFC<T, P>(Component as RefFC<T, P>, props)
+    return styledRefFC<T, P>(
+      Component as RefFC<T, P>,
+      attrs as StyledRefFCAttrs<T, P>,
+    )
   } else {
-    return styledFC<T>(Component as FC<T>, props)
+    return styledFC<T>(Component as FC<T>, attrs as StyledFCAttrs<T>)
   }
 }
 
-function styledFC<T>(Component: FC<T>, props?: Partial<FCProps<T>>) {
-  return function (newProps: FCProps<T>) {
+function styledFC<T>(Component: FC<T>, attrs?: StyledFCAttrs<T>) {
+  return function (props: FCProps<T>) {
+    const computedAttrs = computeAttrs<FCProps<T>>(props, attrs)
+
     return React.createElement(Component, {
+      ...computedAttrs,
       ...props,
-      ...newProps,
     })
   }
 }
 
 function styledRefFC<T, P>(
   Component: RefFC<T, P>,
-  props?: Partial<RefFCProps<T, P>>,
+  attrs?: StyledRefFCAttrs<T, P>,
 ) {
-  function NewComponent(newProps: T, ref: React.ForwardedRef<P>) {
-    return React.createElement<T>(Component, { ...props, ...newProps, ref })
+  function NewComponent(
+    props: React.PropsWithChildren<T>,
+    ref: React.ForwardedRef<P>,
+  ) {
+    const computedAttrs = computeAttrs<RefFCProps<T, P>>(props, attrs)
+
+    return React.createElement(Component, {
+      ...computedAttrs,
+      ...props,
+      ref,
+    })
   }
 
   return React.forwardRef(NewComponent)
