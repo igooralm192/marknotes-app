d08a4eed8dfd69849d50ed968fc6a675d127f0a0
Igor de Almeida <igoor1205@gmail.com>
2021-12-29 11:47:13 -0300

    :hammer: Accept undefined dates on formatDate helper

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,8 +1,8 @@
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 
-export const formatDate = (date: string): string => {
-  const parsedDate = parseISO(date)
+export const formatDate = (date?: string): string => {
+  const parsedDate = parseISO(date || new Date().toISOString())
   return format(parsedDate, "dd 'de' LLLL 'de' yyyy', às 'HH:mm'h'", {
     locale: ptBR,
   })
